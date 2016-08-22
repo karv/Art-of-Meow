1b9c7331b08c3a0b33dd5f3a786ff45875b1696a
Edgar Carballo <karvayoEdgar@gmail.com>
2016-08-22 17:25:46 -0500

    GetDirection extensión

--- Units/MovementDirectionEnum.cs
+++ Units/MovementDirectionEnum.cs
@@ -14,11 +14,42 @@ namespace Units
 		DownLeft,
 		Down,
 		DownRight,
-		__total_dirs
+		__total_dirs,
+		NoMov
 	}
 
 	public static class Movement
 	{
+		public static MovementDirectionEnum GetDirectionTo (this Point @from,
+		                                                    Point to)
+		{
+			var pdir = to - from;
+			return pdir.GetDirection ();
+		}
+
+		static MovementDirectionEnum GetDirection (this Point point)
+		{
+			if (point.X > 0)
+				return point.Y > 0 ?
+					MovementDirectionEnum.DownRight :
+					point.Y == 0 ? 
+					MovementDirectionEnum.Right :
+					MovementDirectionEnum.UpRight;
+
+			if (point.X == 0)
+				return point.Y > 0 ?
+					MovementDirectionEnum.Down :
+					point.Y == 0 ? 
+					MovementDirectionEnum.NoMov :
+					MovementDirectionEnum.Up;
+
+			return point.Y > 0 ?
+					MovementDirectionEnum.DownLeft :
+					point.Y == 0 ? 
+					MovementDirectionEnum.Left :
+					MovementDirectionEnum.UpLeft;
+		}
+
 		/// <summary>
 		/// Rota la dirección rotation*45° hacia la izquierda.
 		/// </summary>
@@ -28,8 +59,6 @@ namespace Units
 			var int_dir = ((int)dir + rotation) % (int)MovementDirectionEnum.__total_dirs;
 			return (MovementDirectionEnum)int_dir;
 		}
-
-
 
 		public static Point AsDirectionalPoint (this MovementDirectionEnum dir)
 		{
