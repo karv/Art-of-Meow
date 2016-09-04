ef22a8da822f9f23f80e3baa0cf28620731b5124
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-04 17:56:16 -0500

    Agregar teclas de diagonal (para teclados sin numpad)

--- Units/Inteligencia/HumanIntelligence.cs
+++ Units/Inteligencia/HumanIntelligence.cs
@@ -31,30 +31,38 @@ namespace Units.Inteligencia
 			{
 				case Microsoft.Xna.Framework.Input.Keys.Down:
 				case Microsoft.Xna.Framework.Input.Keys.NumPad2:
+				case Microsoft.Xna.Framework.Input.Keys.K:
 					ActionDir = MovementDirectionEnum.Down;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.Right:
 				case Microsoft.Xna.Framework.Input.Keys.NumPad6:
+				case Microsoft.Xna.Framework.Input.Keys.O:
 					ActionDir = MovementDirectionEnum.Right;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.Up:
 				case Microsoft.Xna.Framework.Input.Keys.NumPad8:
+				case Microsoft.Xna.Framework.Input.Keys.D8:
 					ActionDir = MovementDirectionEnum.Up;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.Left:
 				case Microsoft.Xna.Framework.Input.Keys.NumPad4:
+				case Microsoft.Xna.Framework.Input.Keys.U:
 					ActionDir = MovementDirectionEnum.Left;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.NumPad1:
+				case Microsoft.Xna.Framework.Input.Keys.J:
 					ActionDir = MovementDirectionEnum.DownLeft;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.NumPad3:
+				case Microsoft.Xna.Framework.Input.Keys.L:
 					ActionDir = MovementDirectionEnum.DownRight;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.NumPad7:
+				case Microsoft.Xna.Framework.Input.Keys.D7:
 					ActionDir = MovementDirectionEnum.UpLeft;
 					return true;
 				case Microsoft.Xna.Framework.Input.Keys.NumPad9:
+				case Microsoft.Xna.Framework.Input.Keys.D9:
 					ActionDir = MovementDirectionEnum.UpRight;
 					return true;
 			}
