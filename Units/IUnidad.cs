b2bca8ffd2b3da5858d7217f2aea07e775f27e97
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-05 16:47:58 -0500

    IUnidad.DoDamage se encarga de hacer recibir daño. Opcionalmente muestra el daño como un VanishingString

--- Units/IUnidad.cs
+++ Units/IUnidad.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Cells;
 using Cells.CellObjects;
+using Componentes;
+using Moggle.Screens;
 using Units.Recursos;
 
 namespace Units
@@ -30,6 +32,23 @@ namespace Units
 		public static void Die (this IUnidad u)
 		{
 			u.MapGrid.RemoveObject (u);
+		}
+
+		public static void DoDamage (this IUnidad u,
+		                             float dmg)
+		{
+			var hp = u.Recursos.GetRecurso ("hp");
+			hp.Valor -= dmg;
+
+		}
+
+		public static void DoDamage (this IUnidad u,
+		                             float dmg, IScreen scr)
+		{
+			const TimeSpan duración = TimeSpan.FromMilliseconds (300);
+			DoDamage (u, dmg);
+			var vs = new VanishingString (scr, dmg, duración);
+			scr.AddComponent (vs);
 		}
 	}
 }
