5f9e3c619335717203cfbb1bd370c120c4c0ae7a
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-08 17:17:53 -0500

    doc

--- Units/Recursos/StatRecurso.cs
+++ Units/Recursos/StatRecurso.cs
@@ -3,6 +3,9 @@ using Microsoft.Xna.Framework;
 
 namespace Units.Recursos
 {
+	/// <summary>
+	/// Recurso genérico de stats
+	/// </summary>
 	public class StatRecurso : IRecurso
 	{
 		#region IUpdate implementation
@@ -29,20 +32,40 @@ namespace Units.Recursos
 
 		#region IRecurso implementation
 
+		/// <summary>
+		/// Nombre (debe ser único en el manejador de recursos) del recurso
+		/// </summary>
+		/// <value>The nombre único.</value>
 		public string NombreÚnico { get; }
 
+		/// <summary>
+		/// Nombre corto
+		/// </summary>
+		/// <value>The nombre corto.</value>
 		public string NombreCorto { get; set; }
 
+		/// <summary>
+		/// Nombre largo
+		/// </summary>
+		/// <value>The nombre largo.</value>
 		public string NombreLargo { get; set; }
 
 		readonly float [] currMaxNormal = new float[3];
 
+		/// <summary>
+		/// Valor actual del recurso.
+		/// </summary>
+		/// <value>The valor.</value>
 		public float Valor
 		{ 
 			get { return currMaxNormal [0]; } 
 			set	{ currMaxNormal [0] = Math.Min (value, Max); }
 		}
 
+		/// <summary>
+		/// Devuelve o establece el máximo actual
+		/// </summary>
+		/// <value>The max.</value>
 		public float Max
 		{ 
 			get { return currMaxNormal [1]; } 
@@ -53,6 +76,10 @@ namespace Units.Recursos
 			}
 		}
 
+		/// <summary>
+		/// Devuelve o establece el valor base.
+		/// </summary>
+		/// <value>The base.</value>
 		public float Base
 		{ 
 			get { return currMaxNormal [2]; } 
@@ -65,14 +92,26 @@ namespace Units.Recursos
 
 		}
 
+		/// <summary>
+		/// Devuelve o establece la tasa de recuparación del valor normal respecto al valor máximo
+		/// </summary>
 		public float TasaRecuperaciónNormal { get; set; }
 
+		/// <summary>
+		/// Devuelve o establece la tasa de recuparación del valor máximo respecto al valor base
+		/// </summary>
 		public float TasaRecuperaciónMax { get; set; }
 
+		/// <summary>
+		/// Unidad que posee este recurso.
+		/// </summary>
+		/// <value>The unidad.</value>
 		public IUnidad Unidad { get; }
 
 		#endregion
 
+		/// <param name="nombreÚnico">Nombre único.</param>
+		/// <param name="unidad">Unidad.</param>
 		public StatRecurso (string nombreÚnico, IUnidad unidad)
 		{
 			NombreÚnico = nombreÚnico;
