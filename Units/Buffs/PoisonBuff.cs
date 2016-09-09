46005f07574ae9b85ede665cbfd589c58f7d9859
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-09 16:51:37 -0500

    Agregar PoisonBuff de pruebas

---
+++ Units/Buffs/PoisonBuff.cs
@@ -0,0 +1,17 @@
+﻿using Units.Recursos;
+
+namespace Units.Buffs
+{
+	public class PoisonBuff : Buff
+	{
+		public override string BaseTextureName { get { return "poison_buff"; } }
+
+		public override string Nombre { get { return "Poison"; } }
+
+		public override void Update (float gameTime)
+		{
+			var hp = Manager.HookedOn.Recursos.GetRecurso (ConstantesRecursos.HP);
+			hp.Valor -= gameTime * 0.1f;
+		}
+	}
+}
