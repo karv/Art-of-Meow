1a83b93b1acd979eec58c7712cbabc58e2d846bf
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-17 17:09:24 -0500

    Agregar interface de acumulación de buff de equipo y el acumulador EquipBuff

---
+++ Units/Buffs/EquipBuff.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using Items;
+using Units.Equipment;
+using Units.Recursos;
+
+namespace Units.Buffs
+{
+	public class EquipBuff : IBuff
+	{
+		Dictionary<string, float> delta { get; }
+
+		/// <summary>
+		/// Devuelve el Equipment de la unidad
+		/// </summary>
+		public EquipmentManager EquipManager { get; }
+
+		public BuffManager BuffManager { get; }
+
+		public ManejadorRecursos RecManager { get; }
+
+		#region IBuff implementation
+
+		/// <summary>
+		/// Se invoca cuando está por desanclarse
+		/// </summary>
+		public void Terminating ()
+		{
+			foreach (var x in delta)
+			{
+				
+			}
+			throw new NotImplementedException ();
+		}
+
+		/// <summary>
+		/// De invoca justo después de anclarse.
+		/// </summary>
+		public void Initialize ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		/// <summary>
+		/// Nombre
+		/// </summary>
+		/// <value>The nombre.</value>
+		public string Nombre
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		/// <summary>
+		/// Devuelve la textura a usar
+		/// </summary>
+		/// <value>The name of the base texture.</value>
+		public string BaseTextureName
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public BuffManager Manager
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		#endregion
+
+		#region IInternalUpdate implementation
+
+		public void Update (float gameTime)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		public EquipBuff ()
+		{
+		}
+	}
+}
+
---
+++ Units/Buffs/IBuffGenerating.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Units.Buffs
+{
+	/// <summary>
+	/// Es un objeto que puede ir acumulando y generando un buff.
+	/// ie Equipment que cambia estado
+	/// </summary>
+	public interface IBuffGenerating
+	{
+		/// <summary>
+		/// Enumera los stats y la cantidad que son modificados.
+		/// </summary>
+		IEnumerable<KeyValuePair<string, float>> GetDeltaStat ();
+	}
+}
