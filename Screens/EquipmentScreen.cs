626eecc6e73e4c919e1f80ccd219f851dfd747a1
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-21 18:05:56 -0500

    Agregar clase EquipmentScreen : ScreenDial

---
+++ Screens/EquipmentScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Moggle.Screens;
+
+namespace Screens
+{
+	public class EquipmentScreen : DialScreen
+	{
+		public EquipmentScreen (IScreen baseScreen)
+			: base (baseScreen.Juego,
+			        baseScreen)
+		{
+		}
+	}
+}
