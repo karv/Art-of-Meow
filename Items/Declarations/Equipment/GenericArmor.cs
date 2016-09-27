b6fd02cee35bc1664cc5552333bee797b243258c
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-26 19:03:56 -0500

    Agregar clase abstract Armor: GenericArmor

---
+++ Items/Declarations/Equipment/GenericArmor.cs
@@ -0,0 +1,22 @@
+﻿using Units.Buffs;
+using System.Collections.Generic;
+
+namespace Items.Declarations.Equipment
+{
+	public abstract class GenericArmor : Equipment, IBuffGenerating
+	{
+		public Dictionary<string, float> DeltaDict { get; }
+
+		public IEnumerable<KeyValuePair<string, float>> GetDeltaStat ()
+		{
+			return DeltaDict;
+		}
+
+		protected GenericArmor (string nombre)
+			: base (nombre)
+		{
+			DeltaDict = new Dictionary<string, float> ();
+		}
+		
+	}
+}
