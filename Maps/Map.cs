ab7f7ada4f7092c9a0da1de042426bc31085993f
Edgar Carballo <karvayoEdgar@gmail.com>
2016-09-28 11:16:15 -0500

    Grid.ctor no se encarga de hacer el mapa

--- Maps/Map.cs
+++ Maps/Map.cs
@@ -4,6 +4,7 @@ using Moggle.Screens;
 using Cells.CellObjects;
 using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Maps
 {
@@ -18,6 +19,33 @@ namespace Maps
 				_data.GetLength (1)); } }
 
 		readonly char [,] _data;
+		readonly Random _r;
+
+		public void AddBoundsTo (char c, Grid grid)
+		{
+			foreach (var pt in contorno())
+				grid.AddCellObject (MakeObject (c, grid, pt));
+		}
+
+		/// <summary>
+		/// enumera las celdas de contorno.
+		/// </summary>
+		IEnumerable<Point> contorno ()
+		{
+			int sizeX = MapSize.Width;
+			int sizeY = MapSize.Height;
+			for (int i = 0; i < sizeX; i++)
+			{
+				yield return (new Point (i, 0));
+				yield return (new Point (i, sizeY - 1));
+			}
+			for (int i = 1; i < sizeY - 1; i++)
+			{
+				yield return (new Point (0, i));
+				yield return (new Point (sizeX - 1, i));
+			}
+		}
+
 
 		/// <summary>
 		/// Generates a <see cref="Grid"/>
@@ -44,7 +72,7 @@ namespace Maps
 				case ' ':
 					return new BackgroundObject (p, "floor", grid);
 				case 'W':
-					var newObj = new GridObject ("brick-wall");
+					var newObj = new GridObject ("brick-wall", grid);
 					newObj.Depth = Depths.Foreground;
 					newObj.CollidePlayer = true;
 					newObj.UseColor = Color.DarkGray;
@@ -54,25 +82,45 @@ namespace Maps
 			throw new Exception ("Unknown map symbol " + c);
 		}
 
+		public void AddRandomFlavorFeatures (Grid grid)
+		{
+			const float probZacate = 0.1f;
+			for (int ix = 0; ix < MapSize.Width; ix++)
+				for (int iy = 0; iy < MapSize.Height; iy++)
+					if (_r.NextDouble () < probZacate)
+					{
+						var newObj = new GridObject ("vanilla-flower", grid);
+						newObj.Location = new Point (ix, iy);
+						newObj.Depth = Depths.GroundDecoration;
+						newObj.CollidePlayer = false;
+						newObj.UseColor = Color.Green;
+						grid.AddCellObject (newObj);
+					}
+		}
+
 		public Map (char [,] data)
 		{
+			_r = new Random ();
 			_data = new char[data.GetLength (0), data.GetLength (1)];
 			data.CopyTo (_data, 0);
 		}
 
 		public Map ()
 		{
+			_r = new Random ();
 			_data = new char[0, 0];
 		}
 
 		public Map (Size size)
 		{
+			_r = new Random ();
 			_data = new char[size.Width, size.Height];
 		}
 
 		public Map (string fileName)
 		{
-			
+			_r = new Random ();
+			// TODO
 		}
 
 	}
