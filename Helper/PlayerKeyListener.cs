003e0d8b31457254bb0b37a0af53712e61798ccf
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-12 15:08:55 -0600

    Clase se encarga de hotkey especiales

---
+++ Helper/PlayerKeyListener.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.Xna.Framework.Input;
+using Moggle;
+using Moggle.Comm;
+using Moggle.Controles;
+using MonoGame.Extended.InputListeners;
+using Screens;
+using Units;
+
+namespace Helper
+{
+	public class PlayerKeyListener : IReceptor<KeyboardEventArgs>, IComponent
+	{
+		Unidad HumanPlayer { get { return ManagerScreen.Player; } }
+
+		public MapMainScreen ManagerScreen { get; }
+
+		Game Juego { get { return ManagerScreen.Juego; } }
+
+
+		#region IReceptor implementation
+
+		public bool RecibirSeñal (KeyboardEventArgs keyArg)
+		{
+			var key = keyArg.Key;
+
+			switch (key)
+			{
+				case Keys.Escape:
+					Juego.Exit ();
+					break;
+				case Keys.Tab:
+					Debug.WriteLine (HumanPlayer.Recursos);
+					break;
+				case Keys.I:
+					if (HumanPlayer.Inventory.Any () || HumanPlayer.Equipment.EnumerateEquipment ().Any ())
+					{
+						var scr = new EquipmentScreen (ManagerScreen, HumanPlayer);
+						scr.Ejecutar ();
+					}
+					break;
+				case Keys.C:
+					ManagerScreen.GridControl.TryCenterOn (HumanPlayer.Location);
+					break;
+				case Keys.S:
+					if (HumanPlayer.Skills.AnyUsable ())
+					{
+						var scr = new InvokeSkillListScreen (ManagerScreen, HumanPlayer);
+						scr.Ejecutar ();
+					}
+					break;
+			}
+			return false;
+		}
+
+		#endregion
+
+		
+		#region IComponent implementation
+
+		public void AddContent ()
+		{
+		}
+
+		public void InitializeContent ()
+		{
+		}
+
+		#endregion
+
+		#region IGameComponent implementation
+
+		public void Initialize ()
+		{
+		}
+
+		#endregion
+
+		
+		public PlayerKeyListener (MapMainScreen scr)
+		{
+			ManagerScreen = scr;
+		}
+	}
+}
+
--- Screens/MapMainScreen.cs
+++ Screens/MapMainScreen.cs
@@ -39,6 +39,8 @@ namespace Screens
 		/// </summary>
 		public BuffDisplay _playerHooks { get; private set; }
 
+		PlayerKeyListener SpecialKeyListener;
+
 		/// <summary>
 		/// Devuelve el tablero logico.
 		/// </summary>
@@ -173,6 +175,8 @@ namespace Screens
 			// los nuevos objetos.
 			base.Initialize ();
 			GridControl.TryCenterOn (Player.Location);
+
+			SpecialKeyListener = new PlayerKeyListener (this);
 		}
 
 		/// <summary>
@@ -206,34 +210,11 @@ namespace Screens
 		/// </summary>
 		protected void TeclaPresionada (KeyboardEventArgs keyArg)
 		{
+			if (SpecialKeyListener.RecibirSeñal (keyArg))
+				return;
+			
 			var key = keyArg.Key;
 
-			switch (key)
-			{
-				case Keys.Escape:
-					Juego.Exit ();
-					break;
-				case Keys.Tab:
-					Debug.WriteLine (Player.Recursos);
-					break;
-				case Keys.I:
-					if (Player.Inventory.Any () || Player.Equipment.EnumerateEquipment ().Any ())
-					{
-						var scr = new EquipmentScreen (this, Player);
-						scr.Ejecutar ();
-					}
-					break;
-				case Keys.C:
-					GridControl.TryCenterOn (Player.Location);
-					break;
-				case Keys.S:
-					if (Player.Skills.AnyUsable ())
-					{
-						var scr = new InvokeSkillListScreen (this, Player);
-						scr.Ejecutar ();
-					}
-					break;
-			}
 			var playerCell = Grid.GetCell (Player.Location);
 			foreach (var x in playerCell.Objects.OfType<IReceptor<KeyboardEventArgs>> ())
 				x.RecibirSeñal (keyArg);
