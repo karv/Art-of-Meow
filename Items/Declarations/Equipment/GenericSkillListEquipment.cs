c6f61d841c88217ad28a3f2a23ff83e9461a17e6
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-14 16:15:31 -0600

    Agregar equipment genérico de invocación de skills y skill (incompleto) de ataque de rando

---
+++ Items/Declarations/Equipment/GenericSkillListEquipment.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Units.Skills;
+
+namespace Items.Declarations.Equipment
+{
+	/// <summary>
+	/// Arma genérica de rango
+	/// </summary>
+	public class GenericSkillListEquipment : Equipment, ISkillEquipment
+	{
+		/// <summary>
+		/// Gets the slot where it can be equiped.
+		/// </summary>
+		public override EquipSlot Slot { get; }
+
+		/// <summary>
+		/// Devuelve el skill que se invoca al usar este item
+		/// </summary>
+		public IEnumerable <ISkill> InvokedSkills { get; }
+
+		IEnumerable<ISkill> ISkillEquipment.GetEquipmentSkills ()
+		{
+			return InvokedSkills;
+		}
+
+		/// <summary>
+		/// </summary>
+		/// <param name="nombre">Nombre.</param>
+		/// <param name="invokedSkill">Invoked skill list.</param>
+		public GenericSkillListEquipment (string nombre,
+		                                  IEnumerable<ISkill> invokedSkill)
+			: base (nombre)
+		{
+			InvokedSkills = invokedSkill;
+		}
+	}
+}
---
+++ Items/Declarations/Equipment/Skills/RangedSkill.cs
@@ -0,0 +1,78 @@
+﻿using AoM;
+using Cells;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Moggle.Controles;
+using Units.Skills;
+
+namespace Items.Declarations.Equipment.Skills
+{
+	/// <summary>
+	/// Skill de ataque de rango de esta clase.
+	/// </summary>
+	public class RangedDamage : ISkill
+	{
+		/// <summary>
+		/// Ejecuta
+		/// </summary>
+		/// <param name="user">Usuario</param>
+		public void Execute (Units.IUnidad user)
+		{
+			// TODO: Seleccionar target
+		}
+
+		/// <summary>
+		/// Determines whether this skill is castable by the specified user.
+		/// </summary>
+		/// <param name="user">User</param>
+		public bool IsCastable (Units.IUnidad user)
+		{
+			// TODO: Debe consumir(requerir) ¿ammo?
+			return true;
+		}
+
+		/// <summary>
+		/// Determines whether this instance is visible the specified user.
+		/// </summary>
+		/// <param name="user">User.</param>
+		public bool IsVisible (Units.IUnidad user)
+		{
+			return true;
+		}
+
+		#region Contenido
+
+		/// <summary>
+		/// Gets or sets the name of the texture.
+		/// </summary>
+		/// <value>The name of the texture.</value>
+		public string TextureName { get; set; }
+
+		/// <summary>
+		/// Gets the icon.
+		/// </summary>
+		/// <value>The icon.</value>
+		protected Texture2D Icon { get; private set; }
+
+		void IComponent.AddContent ()
+		{
+			Program.MyGame.Contenido.AddContent (TextureName);
+		}
+
+		void IComponent.InitializeContent ()
+		{
+			Icon = Program.MyGame.Contenido.GetContent<Texture2D> (TextureName);
+		}
+
+		#endregion
+
+		void IGameComponent.Initialize ()
+		{
+		}
+
+		void IDibujable.Draw (SpriteBatch bat, Rectangle rect)
+		{
+			bat.Draw (Icon, destinationRectangle: rect, layerDepth: Depths.SkillIcon);
+		}
+	}
+}
