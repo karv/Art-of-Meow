03a90762298c1330d8a8e3a0adfa6ef14cee5b4b
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-14 16:26:19 -0600

    Agregar pantalla de selección de target

---
+++ Screens/SelectTargetScreen.cs
@@ -0,0 +1,41 @@
+﻿using Cells;
+using Componentes;
+using Moggle.Screens;
+
+namespace Screens
+{
+	public class SelectableGridControl : GridControl
+	{
+		public SelectableGridControl (LogicGrid grid, IScreen scr)
+			: base (grid, scr)
+		{
+		}
+	}
+
+	public class SelectTargetScreen : DialScreen
+	{
+		// No dibujar base, hay que dibujar otro tipo-GridControl más apropiado
+		public override bool DibujarBase { get { return false; } }
+
+		public LogicGrid Grid { get; }
+
+		public SelectableGridControl GridSelector { get; }
+
+		public override void Initialize ()
+		{
+			base.Initialize ();
+
+			// Poner a GridSelector donde debe
+			// TODO?
+		}
+
+		public SelectTargetScreen (IScreen baseScreen, LogicGrid grid)
+			: base (baseScreen.Juego, baseScreen)
+		{
+			Grid = grid;
+			GridSelector = new SelectableGridControl (Grid, this);
+			AddComponent (GridSelector);
+		}
+		
+	}
+}
