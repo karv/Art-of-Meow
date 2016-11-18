ad6075b50a5262867621458583c011886af9d2e7
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-18 15:59:23 -0600

    Agregar el framework de Effects

---
+++ Skills/IEffect.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Skills
+{
+	/// <summary>
+	/// Un objeto que puede producir un <see cref="IEffect"/>
+	/// </summary>
+	public interface IEffectAgent
+	{
+		
+	}
+
+	/// <summary>
+	/// Un target de un <see cref="ITargetEffect"/>
+	/// </summary>
+	public interface ITarget
+	{
+		
+	}
+
+	/// <summary>
+	/// Un efecto de un objeto.
+	/// </summary>
+	public interface IEffect
+	{
+		IEffectAgent Agent { get; }
+
+		void Execute ();
+	}
+
+	/// <summary>
+	/// Un efecto que tiene un target
+	/// </summary>
+	public interface ITargetEffect : IEffect
+	{
+		ITarget Target { get; }
+	}
+}
