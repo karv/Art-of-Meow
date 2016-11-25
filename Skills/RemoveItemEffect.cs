e00f7dfc89630f509e230115f192e41527697805
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-25 16:05:13 -0600

    Clases en su archivo

---
+++ Skills/RemoveItemEffect.cs
@@ -0,0 +1,68 @@
+using System;
+using Items;
+using Skills;
+using Units;
+
+namespace Skills
+{
+	/// <summary>
+	/// Representa el efecto de un <see cref="ISkill"/> que consisnte en eliminar un item de un <see cref="IUnidad"/>
+	/// </summary>
+	public class RemoveItemEffect : ITargetEffect
+	{
+		/// <summary>
+		/// Runs the effect
+		/// </summary>
+		public void Execute ()
+		{
+			// Removes the item, it it does not exists: exception
+			if (!Target.Inventory.Items.Remove (RemovingItem))
+				throw new Exception ("Cannot execute effect");
+		}
+
+		/// <summary>
+		/// Devuelve un <c>string</c> de una línea que describe este efecto como infobox
+		/// </summary>
+		public string DetailedInfo ()
+		{
+			return string.Format ("{1}: Removes {0}", RemovingItem.Nombre, Chance);
+		}
+
+		/// <summary>
+		/// Probabilidad de que ocurra
+		/// </summary>
+		public double Chance { get; set; }
+
+		/// <summary>
+		/// Whose inventory gonna lose the item.
+		/// </summary>
+		public IUnidad Target { get; }
+
+		ITarget ITargetEffect.Target { get { return Target; } }
+
+		/// <summary>
+		/// Item to remove
+		/// </summary>
+		public IItem RemovingItem { get; }
+
+		/// <summary>
+		/// What caused this effect
+		/// </summary>
+		public IEffectAgent Agent { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Skills.RemoveItemEffect"/> class.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="target">Target.</param>
+		/// <param name="removingItem">Removing item.</param>
+		public RemoveItemEffect (IEffectAgent source,
+		                         IUnidad target,
+		                         IItem removingItem)
+		{
+			Agent = source;
+			Target = target;
+			RemovingItem = removingItem;
+		}
+	}
+}
---
+++ Skills/SkillInstance.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using Skills;
+using Units.Skills;
+
+namespace Skills
+{
+
+	/// <summary>
+	/// Una instancia de skill.
+	/// Se usa como feedback de datos y resultados del skill
+	/// </summary>
+	public class SkillInstance
+	{
+		/// <summary>
+		/// Tipo de skill
+		/// </summary>
+		public ISkill Skill { get; }
+
+		/// <summary>
+		/// El agente o usuario del skill
+		/// </summary>
+		public IEffectAgent Agent { get; }
+
+		readonly List<IEffect> _effects;
+
+		/// <summary>
+		/// Añadir un efecto
+		/// </summary>
+		public void AddEffect (IEffect effect)
+		{
+			_effects.Add (effect);
+		}
+
+		/// <summary>
+		/// Ejecuta
+		/// </summary>
+		public void Execute ()
+		{
+			for (int i = 0; i < _effects.Count; i++)
+				_effects [i].Execute ();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Skills.SkillInstance"/> class.
+		/// </summary>
+		/// <param name="skill">Skill.</param>
+		/// <param name="agent">Agent.</param>
+		public SkillInstance (ISkill skill, IEffectAgent agent)
+		{
+			Skill = skill;
+			Agent = agent;
+			_effects = new List<IEffect> ();
+		}
+	}
+}
