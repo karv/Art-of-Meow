e466ac290830568ae58424c059a4db64752b6186
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-28 16:15:41 -0600

    Agregar clase que representa una modificación de item

---
+++ Items/ItemModifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Items
+{
+	public enum ItemModifierNameUsage
+	{
+		Prefix,
+		Sufix
+	}
+
+	public sealed class ItemModifier
+	{
+		public ItemModifierNameUsage NameUsage { get; }
+
+		public string Name { get; }
+
+		public string ApplyToName (string name)
+		{
+			switch (NameUsage)
+			{
+				case ItemModifierNameUsage.Prefix:
+					return Name + " " + name;
+				case ItemModifierNameUsage.Sufix:
+					return name + " " + Name;
+			}
+			throw new Exception ("Unknown usage type");
+		}
+
+		public void ApplyToItem (IItem item)
+		{
+		}
+	}
+}
