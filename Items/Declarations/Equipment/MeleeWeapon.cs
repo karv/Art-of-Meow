7468ed07cc3ae47c640a1d527d535cfd83762032
Edgar Carballo <karvayoEdgar@gmail.com>
2016-11-30 15:41:18 -0600

    doc

--- Items/Declarations/Equipment/MeleeWeapon.cs
+++ Items/Declarations/Equipment/MeleeWeapon.cs
@@ -5,11 +5,21 @@ using Units;
 
 namespace Items.Declarations.Equipment
 {
-
+	/// <summary>
+	/// Representa una arma melee genérica
+	/// </summary>
 	public class MeleeWeapon : Equipment, IMeleeEffect
 	{
+		/// <summary>
+		/// Devuelve el daño base
+		/// </summary>
+		/// <value>The base damage.</value>
 		public float BaseDamage { get; }
 
+		/// <summary>
+		/// Devuelve la certeza base
+		/// </summary>
+		/// <value>The base hit.</value>
 		public float BaseHit { get; }
 
 		#region IEquipment implementation
