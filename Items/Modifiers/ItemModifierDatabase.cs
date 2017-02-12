e813f159ccb9a79c54bd975818c408e431a0bcfc
Edgar Carballo <karvayoedgar@gmail.com>
2017-02-12 16:04:08 -0600

    Small comilation fix

--- Items/Modifiers/ItemModifierDatabase.cs
+++ Items/Modifiers/ItemModifierDatabase.cs
@@ -24,7 +24,7 @@ namespace Items.Modifiers
 		{
 			get
 			{
-				return Mods.First (z => string.Compare (z.Name, name, true));
+				return Mods.First (z => string.Compare (z.Name, name, true) == 0);
 			}
 		}
 
