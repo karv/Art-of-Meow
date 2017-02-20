3cd8399d9c1713923ed056c937b6f8d8d3570dc1
Edgar Carballo <karvayoedgar@gmail.com>
2017-02-20 13:36:59 -0600

    Add partially the class DamageAttribute

---
+++ DamageAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace AoM
+{
+	/// <summary>
+	/// Represents an attribute (elemental, etc) as damage type
+	/// </summary>
+	public class DamageAttribute : IIdentificable
+	{
+		public string Name { get; }
+
+		[JsonConstructor]
+		DamageAttribute (string Name)
+		{
+			this.Name = Name;
+		}
+	}
+}
