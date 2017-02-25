6d60faff6cf5bd78d7e74108f8e08df1f8f07d72
Edgar Carballo <karvayoedgar@gmail.com>
2017-02-25 16:45:49 -0600

    doc

--- Items/Declarations/Arrow.cs
+++ Items/Declarations/Arrow.cs
@@ -3,25 +3,49 @@ using Skills;
 
 namespace Items.Declarations
 {
+	/// <summary>
+	/// An arrow
+	/// </summary>
 	public class Arrow : StackingItem, IEquipment, IEffectAgent
 	{
+		/// <summary>
+		/// Gets the slot where it can be equiped
+		/// </summary>
+		/// <value>The slot.</value>
 		public EquipSlot Slot
 		{
 			get { return EquipSlot.Quiver; }
 		}
 
+		/// <summary>
+		/// The damage mutiplier
+		/// </summary>
 		public float DamageMultiplier = 1;
+		/// <summary>
+		/// The base hit chance
+		/// </summary>
 		public float BaseHit = 0.7f;
 
-		public Units.Equipment.EquipmentManager Owner { get; set; }
+		/// <summary>
+		/// The name of the damage attribute
+		/// </summary>
+		public string Attribute;
 
+		Units.Equipment.EquipmentManager IEquipment.Owner { get; set; }
+
+		/// <summary>
+		/// Clone this instance.
+		/// </summary>
 		public override object Clone ()
 		{
 			return new Arrow (NombreBase, AllowedModNames)
-			{ Quantity = Quantity, Attribute = Attribute };
+			{
+				Quantity = Quantity, 
+				Attribute = Attribute,
+				DamageMultiplier = DamageMultiplier, 
+				BaseHit = BaseHit
+			};
 		}
-
-		public string Attribute;
 
 		[JsonConstructor]
 		Arrow (string nombre, string [] allowedModNames)
--- Items/IStackingItem.cs
+++ Items/IStackingItem.cs
@@ -2,8 +2,14 @@
 
 namespace Items
 {
+	/// <summary>
+	/// Represents an item that is stacked several times while sharing all the properties
+	/// </summary>
 	public interface IStackingItem : IItem
 	{
+		/// <summary>
+		/// Quantity of items
+		/// </summary>
 		int Quantity { get; set; }
 	}
 }
